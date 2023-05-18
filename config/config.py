d87402d27d00f49a3f389769f525243fb3b2663e
Lucky Raja <129265896+LuckyRaja0@users.noreply.github.com>
2023-05-18 21:57:18 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -30,7 +30,7 @@ UPSTREAM_REPO = getenv(
     "https://github.com/itz-star-boi/ShizukaXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/LuckyRaja0/RomeoMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/itz-star-boi/ShizukaXMusic")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ZiddiXBot")
